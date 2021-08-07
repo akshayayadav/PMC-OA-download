a562d21a760b15802a513e9d9d0cd0c7874e2691
akshay <akshayy.yadav@gmail.com>
2021-08-07 13:15:03 +0530

    adding checks for regex and outdir

--- download_pmc_tar.py
+++ download_pmc_tar.py
@@ -7,20 +7,23 @@ def download_PMC_OA_file(pmc_id):
     response = requests.get("https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id="+pmc_id)
     response = response.text
     tar_href = re.search(r'<link\sformat=\"tgz\"\supdated=\".+?\"\shref=\"(.+?)\"', response)
-    tar_href = tar_href.group(1)
-    if(tar_href):
+    if (tar_href):
+        tar_href = tar_href.group(1)
         print(pmc_id, tar_href)
         wget.download(tar_href)
     else:
-        print(pmc_id, "Not Found")
+        print(pmc_id, response)
+
 
 def execute_download(ids_list_filename):
     ids_list_file = open(ids_list_filename, "r")
-    os.mkdir("PMC_downloads")
+    if not os.path.isdir("PMC_downloads"):
+        os.mkdir("PMC_downloads")
     os.chdir("PMC_downloads")
     for line in ids_list_file:
         line = line.rstrip()
         download_PMC_OA_file(line)
+        break
     ids_list_file.close()
 
 execute_download("PMC_ids_list.txt")
